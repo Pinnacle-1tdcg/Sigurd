be9b3c233a7f18bec9ed6d4e0c88c4ff6268c7a2
Pinnacle <114665200+Pinnacle-1tdcg@users.noreply.github.com>
2022-09-29 16:54:53 -0300

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Dependencias para o programa funcionar
+
+sudo apt install python3-pip -y
+sudo pip3 install pyfiglet
+sudo pip3 install --upgrade pyfiglet 
+sudo pip3 install --upgrade psutil
+sudo apt install python3-tk -y
+sudo pip3 install tk
+sudo pip3 install pysimplegui
+sudo pip3 install py_essentials

ff5f6963077fc551b2440192e00184b59eca5c4e
Pinnacle <114665200+Pinnacle-1tdcg@users.noreply.github.com>
2022-09-29 23:21:10 -0300

    Add files via upload

---
+++ requirements_centos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo yum -y install python3
+sudo yum -y install python3-pip -y
+sudo pip3 install pyfiglet
+sudo pip3 install --upgrade pyfiglet 
+sudo pip3 install --upgrade psutil
+
+sudo yum install python3-tkinter -y
+
+sudo pip3 install tk
+sudo pip3 install pysimplegui
+sudo pip3 install py_essentials
+
---
+++ requirements_fedora.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo dnf install python3
+sudo dnf install python3-pip -yum
+sudo pip3 install pyfiglet
+sudo pip3 install --upgrade pyfiglet 
+sudo pip3 install --upgrade psutil
+
+sudo dnf install python3-tkinter -y
+
+sudo pip3 install tk
+sudo pip3 install pysimplegui
+sudo pip3 install py_essentials
